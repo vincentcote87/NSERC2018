08b695bda77d59588cac581413cfb551db476a8c
Julius Moore <julius.moore@uleth.ca>
2018-05-28 13:23:49 -0600

    Hard path taken. About to revert.

--- PsiTracker.cc
+++ PsiTracker.cc
@@ -2,12 +2,16 @@
  
 PsiTracker::PsiTracker()
    : tracker{}
-{}
+{
+    of = 1;
+}
 
 void PsiTracker::bringThetaToTarget(const integer cur_x, const integer i) {
    while (tracker[i].prime() < cur_x) {
-	    tracker[i].next();
+        tracker[i].next();
+	    std::cout << tracker[i].prime() << ' ';
 	 }
+	 std::cout << std::endl;
    if (tracker[i].prime() > cur_x)
       tracker[i].previous();
 }
@@ -23,36 +27,49 @@ void PsiTracker::next_change() {
    do {
       ++of;
    } while (psi() == last);
+   std::cout << "Found next change: " << of << ", " << prime() << std::endl;
 }
 
-integer PsiTracker::count() {
+integer PsiTracker::count() {   
+   if (tracker.empty())
+      return 0;
    return tracker[0].count();
 }
 
 integer PsiTracker::prime() {
+   if (tracker.empty())
+      return 1;
    return tracker[0].prime();
 }
 
 rational PsiTracker::theta() {
+   if (tracker.empty())
+      return 0;
    return tracker[0].theta();
 }
 
 void PsiTracker::populateThetas() {
    integer cur_x;
    for (size_t i = 0; (cur_x = std::pow(of, 1.0/((rational) i+1))) >= 2; ++i) {
+      std::cout << "cur_x: " << cur_x << " i: " << i << " tracker.size(): " << tracker.size() << std::endl;
       if (tracker.size() <= i) {
 	 tracker.push_back(ThetaTracker{});
 	 tracker[i].next();
       }
+      std::cout << "About to call bringThetaToTarget" << std::endl;
+      if (tracker.empty())
+        continue;
       bringThetaToTarget(cur_x, i);
    }
 }
 
 rational PsiTracker::psi() {
+    std::cout << "Entered Psi." << std::endl;
    populateThetas();
+   std::cout << "Populated Thetas" << std::endl;
    //tracker contains the closest prime thetas less than or equal to the target
    rational psi = 0.0;
-   for (size_t i = 0; i < tracker.size(); ++i) { //incorrect
+   for (size_t i = 0; i < tracker.size(); ++i) {
       psi += tracker[i].theta();
    }
    return psi;
--- list_test.cc
+++ list_test.cc
@@ -14,9 +14,10 @@ int main() {
         std::chrono::steady_clock clk;
         std::chrono::time_point<std::chrono::steady_clock, std::chrono::steady_clock::duration> before = clk.now();
         PsiTracker tracker;
-        tracker.next();
+        tracker.next_change();
         while (tracker.prime() < a) {
-            tracker.next();
+            std::cout << tracker.prime() << " < " << a << std::endl;
+            tracker.next_change();
         }
         long long runtime = (std::chrono::duration_cast<std::chrono::milliseconds>(clk.now()-before)).count();
         std::cout << runtime << ',' << tracker.count() << ',' << tracker.prime() << ',' << std::fixed << std::setprecision(PRECISION) << tracker.theta() << ',' << std::fixed << std::setprecision(PRECISION) << tracker.psi() << std::endl;
